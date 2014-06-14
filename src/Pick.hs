db745bcbde14260b38336fd46839c2b6e83b332d
kjgorman <kieran@kjgorman.com>
2014-06-14 22:21:13 +1200

    bleep bloop

--- src/Pick.hs
+++ src/Pick.hs
@@ -5,6 +5,8 @@ module Pick (
 import           Control.Monad (liftM)
 import           System.Random (randomRIO)
 
+-- for some brain dead reason choice in random.extras is a partial fn
+
 pick :: [a] -> IO (Maybe a)
 pick [] = return Nothing
 pick xs = liftM (Just . (xs !!)) $ randomRIO (lo, hi)
--- static/wwrrd.js
+++ static/wwrrd.js
@@ -1,17 +1,16 @@
+/* jshint asi:true, laxcomma:true, expr:true */
 +(function ($) {
+    "use strict";
     var $submit = $('#submit')
       , $output = $('#output')
       , $search = $('#search')
 
     $(submit).click(function () {
-	var query = $search.val()
-	$.getJSON("/query/"+query, function (res) {
+	$.getJSON("/query/"+$search.val(), function (res) {
 	    var json = JSON.parse(res)
 	      , title = json.track.title
 	      , line  = json.phraseLines[0].line
 	    $output.html(line)
 	})
     })
-
-
 }) (Zepto)
