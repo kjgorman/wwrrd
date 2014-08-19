c1ed446e0990a91a4f1d1c65c8904ae3cd188fcc
kjgorman <kieran@kjgorman.com>
2014-08-19 22:19:13 +1200

    i guess that makes sense in a "just drank a bottle of wine and am trying to write lisp on a tuesday kind of way"

--- app/wwrrd/core.cljs
+++ app/wwrrd/core.cljs
@@ -18,29 +18,26 @@
           (put! out (.-value el)))))
     out))
 
-(defn query-submissions [owner]
-  (let* [input (om/get-node owner "query-input")
-         enters (enter-listen input)]
-    (go (while true
-          (let* [query (<! enters)
-                 response (<! (http/get (+ "query/" query) { :with-credentials? false }))]
-            (.writeln js/document (.-line (.parse js/JSON (:body response)))))))))
-
 (defn rick-view [app owner]
   (reify
-   om/IDidMount
-   (did-mount [_]
-     (query-submissions owner))
+    om/IDidMount
+    (did-mount [_]
+      (let* [input (om/get-node owner "query-input")
+             enters (enter-listen input)]
+            (go (while true
+                  (let* [query (<! enters)
+                         response (<! (http/get (+ "query/" query) { :with-credentials? false }))]
+                        (om/transact! app :current (fn [_] (.-line (.parse js/JSON (:body response))))))))))
    om/IRenderState
    (render-state [_ _]
        (dom/h1 nil "wwrrd")
        (dom/div nil
          (dom/div nil "what would rick ross do?")
          (dom/input
-           #js { :type "text" :ref "query-input" })))))
+           #js { :type "text" :ref "query-input" })
+         (dom/div nil (:current app))))))
 
-(def app-state
-  (atom { :current-track nil :query nil }))
+(def app-state (atom { :current "foo" }))
 
 (om/root rick-view app-state
   {:target (. js/document (getElementById "rick"))})
